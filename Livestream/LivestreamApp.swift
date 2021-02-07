14d5c05553d40782849fc6f47c814e36a6548da7
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-02-07 20:31:36 +0800

    Initial Commit

---
+++ Livestream/LivestreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivestreamApp.swift
+//  Livestream
+//
+//  Created by Chloe Fermanis on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LivestreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
