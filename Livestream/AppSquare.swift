a5e8b9750e175f10a8cf2ca70527cd43729fc00d
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-02-07 22:09:50 +0800

    first commit

---
+++ Livestream/AppSquare.swift
@@ -0,0 +1,20 @@
+//
+//  AppSquare.swift
+//  Livestream
+//
+//  Created by Chloe Fermanis on 7/2/21.
+//
+
+import SwiftUI
+
+struct AppSquare: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppSquare_Previews: PreviewProvider {
+    static var previews: some View {
+        AppSquare()
+    }
+}
---
+++ Livestream/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Livestream
+//
+//  Created by Chloe Fermanis on 7/2/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
